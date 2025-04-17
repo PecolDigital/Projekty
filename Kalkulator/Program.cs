377a834c2c19ab509b15380da6f4067904c91403
PecolDigital <marcin3674@o2.pl>
2025-04-17 23:18:43 +0200

    Add files via upload

---
+++ Kalkulator/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kalkulator!");
+
+            double num1, num2, result;
+            string operation;
+
+            Console.Write("Podaj pierwszą liczbę: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Podaj drugą liczbę: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Wybierz operację (+, -, *, /): ");
+            operation = Console.ReadLine();
+
+            switch (operation)
+            {
+                case "+":
+                    result = num1 + num2;
+                    Console.WriteLine($"Wynik: {num1} + {num2} = {result}");
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    Console.WriteLine($"Wynik: {num1} - {num2} = {result}");
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    Console.WriteLine($"Wynik: {num1} * {num2} = {result}");
+                    break;
+                case "/":
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                        Console.WriteLine($"Wynik: {num1} / {num2} = {result}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nie można dzielić przez zero!");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Nieznana operacja.");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
